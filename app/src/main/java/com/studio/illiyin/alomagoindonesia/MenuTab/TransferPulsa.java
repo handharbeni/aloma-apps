cfe8bf2a754b70ae292b8af01501ece282c2c906
handharbeni <mhandharbeni@gmail.com>
2018-02-01 14:45:47 +0700

    Update

--- app/src/main/java/com/studio/illiyin/alomagoindonesia/MenuTab/TransferPulsa.java
+++ app/src/main/java/com/studio/illiyin/alomagoindonesia/MenuTab/TransferPulsa.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.telephony.SmsManager;
@@ -38,7 +39,7 @@ public class TransferPulsa extends Fragment{
     View myView;
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 
         StrictMode.setThreadPolicy(policy);
